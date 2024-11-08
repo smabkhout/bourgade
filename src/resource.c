35ce116f571da56659b8b4efa74f205f806dded9
GUIOT Numa <nguiot@bordeaux-inp.fr>
2024-11-08 14:04:41 +0100

    Modifications sur resource.c

--- src/resource.c
+++ src/resource.c
@@ -27,5 +27,33 @@ const char* resource_to_short_string(enum resource_t r){
 }
 
 
-const char* resource_color_start(enum resource_t r);
+const char* resource_color_start(enum resource_t r){
+  switch (r)
+  {
+  case 0:
+    return "Void";
+  case 1:
+    return "Yellow";
+  case 2:
+    return "Blue";
+  case 3:
+    return "Brown";
+  case 4:
+    return "Grey";
+  case 5:
+    return "Gold";
+  case 6:
+    return "Red";
+  case 7:
+    return "Silver";
+  case 8:
+    return "Cyan";
+  case 9:
+    return "Green";
+  default:
+    return "Unknown";
+  }
+}
+
+
 
