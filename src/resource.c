579a1178db3a825cbff43b0cb24b10509314bf0b
MABKHOUT Salah-Eddine <Salah-Eddine.Mabkhout@bordeaux-inp.fr>
2024-11-08 14:38:21 +0100

    Ajout du fichier resource.c

--- src/resource.c
+++ src/resource.c
@@ -30,30 +30,62 @@ const char* resource_to_short_string(enum resource_t r){
 const char* resource_color_start(enum resource_t r){
   switch (r)
   {
-  case 0:
-    return "Void";
-  case 1:
-    return "Yellow";
-  case 2:
-    return "Blue";
-  case 3:
-    return "Brown";
-  case 4:
-    return "Grey";
-  case 5:
-    return "Gold";
-  case 6:
-    return "Red";
-  case 7:
-    return "Silver";
-  case 8:
-    return "Cyan";
-  case 9:
-    return "Green";
+  case 0: //Void
+    return "\033[38;5;0m";
+  case 1: //Yellow
+    return "\033[38;5;11m";
+  case 2: //Blue
+    return "\033[38;5;12m";
+  case 3: //Brown
+    return "\033[38;5;94m";
+  case 4: //Grey
+    return "\033[38;5;8m";
+  case 5: //Gold
+    return "\033[38;5;172m";
+  case 6: //Red
+    return "\033[0;31m";
+  case 7: //Silver
+    return "\033[38;5;7m";
+  case 8: //Cyan
+    return "\033[38;5;14m";
+  case 9: //Green
+    return "\033[0;32m";
   default:
-    return "Unknown";
+    break;
   }
 }
 
+const char* resource_color_stop(){
+    return "\033[0m";
+}
 
+int resource_le_than(unsigned int r1[NUM_RESOURCES],
+                     unsigned int r2[NUM_RESOURCES]){
+  int i=0;
+  while (i<NUM_RESOURCES){
+    if  (r2[i] > r1[i])
+      return 0;
+    ++i;
+  }
+  return 1;
+}
 
+void resource_add(unsigned int r1[NUM_RESOURCES],
+                  unsigned int r2[NUM_RESOURCES],
+                  unsigned int res[NUM_RESOURCES]){
+  int i=0;
+  while (i<NUM_RESOURCES){
+    res[i] = r1[i] + r2[i];
+    ++i;
+  }
+}
+
+void resource_add(unsigned int r1[NUM_RESOURCES],
+                  unsigned int r2[NUM_RESOURCES],
+                  unsigned int res[NUM_RESOURCES]){
+  int i=0;
+  while (i<NUM_RESOURCES){
+    res[i] = r1[i] - r2[i];
+    ++i;
+  }
+}
