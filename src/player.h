a2584795cd66d41965c27c39135aac8eaebd4a6e
GUIOT Numa <nguiot@bordeaux-inp.fr>
2024-11-10 16:43:36 +0100

    Ajout de player.h

---
+++ src/player.h
@@ -0,0 +1,21 @@
+#include "color.h"
+#include "resource.h"
+
+#ifndef MAX_PLAYERS
+    #define MAX_PLAYERS 4
+#endif
+
+struct player_t{
+    enum color_t color;
+    int stockage[MAX_RESOURCES]; //entrepot representant les reserves du joueur
+};
+
+struct player_t initialize_player(enum color_t couleur){
+    struct player_t player; 
+    player.color = couleur;
+    for (int i = 0; i < MAX_RESOURCES; ++i)
+    {
+        player.stockage[i] = 0;
+    }
+    return player;
+}
