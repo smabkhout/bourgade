ffc58b5529bc66e5e867cb3b95df906b956ac8cc
MABKHOUT Salah-Eddine <Salah-Eddine.Mabkhout@bordeaux-inp.fr>
2024-11-15 15:57:29 +0100

    Ajout de mine.c et mine.h

---
+++ src/mine.c
@@ -0,0 +1,11 @@
+#include "mine.h"
+#include "position.h"
+
+void place_mine(struct cell_t* cell, struct mine_t* mine){
+    cell->mine=mine;
+}
+ srand(seed)
+
+struct mine_t* construct_mine(char* name, enum resource_t r){
+
+}
--- src/mine.h
+++ src/mine.h
@@ -1,6 +1,20 @@
 #include "resource.h"
+#include "board.h"
+#include "position.h"
 
 struct mine_t {
     char* name;
     enum resource_t r;
 };
+
+struct mine_t list_mines[4] = {
+ {.name = "Field", .r=CORN},
+ {.name = "Forest", .r=WOOD},
+ {.name = "River", .r=FISH},
+ {.name = "Rock Mine", .r=STONE},
+};
+
+struct mine_t present_mines[MAX_POSITIONS/4];
+
+void construct_mines();
+
