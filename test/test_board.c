45344c32cda57a8fb5c0c877c7370cfd73f827f9
GUIOT Numa <nguiot@bordeaux-inp.fr>
2024-11-22 14:54:10 +0100

    ajout du fichier test pour le board

---
+++ test/test_board.c
@@ -0,0 +1,52 @@
+#include "board.h"
+#include "cell.h"
+#include "mine.h"
+#include "position.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void test_init_board() {
+    struct board_t* board = init_board();
+    for (int i = 0; i < MAX_X * MAX_Y; ++i) {
+        assert(board->tab[i] != NULL);
+        assert(board->tab[i]->mine == NULL); 
+        assert(board->tab[i]->worker == NULL); 
+        assert(board->tab[i]->building == NULL);
+    }
+    int mine_count = 0;
+    for (int i = 0; i < MAX_X * MAX_Y; ++i) {
+        if (board->tab[i]->mine != NULL) {
+            ++mine_count;
+        }
+    }
+    assert(mine_count == MAX_POSITIONS / 4);
+    int occurrences_result = occurrences(present_mines);
+    assert(occurrences_result != 0); 
+    free_board(board);
+    free(board);        
+}
+
+void test_find_free_cell() {
+    struct board_t* board = init_board();
+    struct cell_t* free_cell = find_free_cell(board);
+    assert(free_cell != NULL);
+    assert(is_free_cell(free_cell) == 1);
+    free_board(board); 
+    free(board);      
+}
+
+void test_free_board() {
+    struct board_t* board = init_board();
+    free_board(board);
+    free(board);
+}
+
+int main() {
+    test_init_board();
+    test_find_free_cell();
+    test_free_board();
+    printf("All board tests passed!\n");
+    return 0;
+}
