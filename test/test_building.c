7c4229e92a7ef63d8750b4576faff3ef79258b3d
GUIOT Numa <nguiot@bordeaux-inp.fr>
2024-11-22 16:14:17 +0100

    ajout des fichiers test

--- test/test_building.c
+++ test/test_building.c
@@ -8,12 +8,11 @@
 #include <stdlib.h>
 
 void test_make_building() {
-    int value[NUM_RESOURCES] = {0, 0, 0, 1, 0, 0};
-    int earns[NUM_RESOURCES] = {0, 0, 0, 0, 0, 3};
-    int costs[NUM_RESOURCES] = {0, 0, 0, 0, 0, 1};
-    int supplies[NUM_RESOURCES] = {0, 1, 0, 0, 0, 0};
+    unsigned int value[NUM_RESOURCES] = {0, 0, 0, 1, 0, 0};
+    unsigned int earns[NUM_RESOURCES] = {0, 0, 0, 0, 0, 3};
+    unsigned int costs[NUM_RESOURCES] = {0, 0, 0, 0, 0, 1};
+    unsigned int supplies[NUM_RESOURCES] = {0, 1, 0, 0, 0, 0};
     char nom[] = "Farm";
-    struct player_t player = { .stockage = {1, 1, 1, 1, 1, 1}, .color = RED };
     struct building_t* building = make_building(nom, value, earns, costs, supplies, RED);
     assert(building != NULL);
     assert(strcmp(building->nom, "Farm") == 0);
@@ -31,10 +30,10 @@ void test_make_building() {
 void test_place_building() {
     struct player_t player = { .stockage = {1, 1, 1, 1, 1, 1}, .color = BLUE };
     struct position_t* position = make_position(1, 1);
-    int value[NUM_RESOURCES] = {0, 0, 0, 1, 0, 0};
-    int earns[NUM_RESOURCES] = {0, 0, 0, 0, 0, 3};
-    int costs[NUM_RESOURCES] = {0, 0, 0, 0, 0, 1};
-    int supplies[NUM_RESOURCES] = {0, 1, 0, 0, 0, 0};
+    unsigned int value[NUM_RESOURCES] = {0, 0, 0, 1, 0, 0};
+    unsigned int earns[NUM_RESOURCES] = {0, 0, 0, 0, 0, 3};
+    unsigned int costs[NUM_RESOURCES] = {0, 0, 0, 0, 0, 1};
+    unsigned int supplies[NUM_RESOURCES] = {0, 1, 0, 0, 0, 0};
     struct building_t* building = make_building("Farm", value, earns, costs, supplies, BLUE);
     place_building(&player, position, building);
     assert(building->position == position);
@@ -50,10 +49,10 @@ void test_activate_building() {
     struct player_t owner = { .stockage = {1, 1, 1, 1, 1, 1}, .color = RED };
     struct player_t user = { .stockage = {5, 5, 5, 5, 5, 5}, .color = BLUE };
     struct position_t* position = make_position(1, 1);
-    int value[NUM_RESOURCES] = {0, 0, 0, 1, 0, 0};
-    int earns[NUM_RESOURCES] = {0, 0, 0, 0, 0, 3};
-    int costs[NUM_RESOURCES] = {0, 0, 0, 0, 0, 1};
-    int supplies[NUM_RESOURCES] = {0, 1, 0, 0, 0, 0};
+    unsigned int value[NUM_RESOURCES] = {0, 0, 0, 1, 0, 0};
+    unsigned int earns[NUM_RESOURCES] = {0, 0, 0, 0, 0, 3};
+    unsigned int costs[NUM_RESOURCES] = {0, 0, 0, 0, 0, 1};
+    unsigned int supplies[NUM_RESOURCES] = {0, 1, 0, 0, 0, 0};
     struct building_t* building = make_building("Farm", value, earns, costs, supplies, RED);
     place_building(&owner, position, building);
     activate_building(&owner, &user, building);
@@ -66,28 +65,36 @@ void test_activate_building() {
 }
 
 void test_list_buildings_costing_less_than() {
-    struct player_t player = { .stockage = {1, 1, 1, 1, 1, 1}, .color = BLUE };
+    struct player_t player;
+    for (int i = 0; i < NUM_RESOURCES; ++i) {
+        player.stockage[i] = i * 10;
+    }
     struct building_t** affordable_buildings = list_buildings_costing_less_than(&player);
-    int count = length_of_affordable_buildings(affordable_buildings);
-    assert(count == 7);
-    for (int i = 0; i < count; ++i) {
-        struct building_t* building = affordable_buildings[i];
-        unsigned int new_stockage[NUM_RESOURCES] = {1};
-        resource_sub(player.stockage, building->value, new_stockage);
-        int is_affordable = 1;
-        for (int j = 0; j < NUM_RESOURCES; ++j) {
-            if (new_stockage[j] < 0) {
-                is_affordable = 0;
-                break;
+    printf("Testing list_buildings_costing_less_than:\n");
+    for (int i = 0; i < 7; ++i) {
+        if (affordable_buildings[i] != NULL) {
+            printf("Affordable building found: %s\n", affordable_buildings[i]->nom);
+            int is_affordable = 1;
+            for (int res = 0; res < NUM_RESOURCES; ++res) {
+                if (affordable_buildings[i]->value[res] > player.stockage[res]) {
+                    is_affordable = 0;
+                    printf("Error: Building '%s' costs more than available resources!\n", affordable_buildings[i]->nom);
+                }
+            }
+            if (is_affordable) {
+                printf("Building '%s' is affordable. Test passed.\n", affordable_buildings[i]->nom);
             }
         }
-        assert(is_affordable == 1);
     }
-    for (int i = 0; i < count; ++i) {
-        free(affordable_buildings[i]);
+    for (int i = 0; i < 7; ++i) {
+        if (affordable_buildings[i] != NULL) {
+            free(affordable_buildings[i]); 
+        }
     }
     free(affordable_buildings);
+    printf("All tests completed.\n");
 }
+
 
 int main() {
     test_make_building();
