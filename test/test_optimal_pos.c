ed09a448f6e27ebda03f90a09245558a90f3f3b5
GUIOT Numa <nguiot@bordeaux-inp.fr>
2024-11-26 18:01:28 +0100

    modif

--- test/test_optimal_pos.c
+++ test/test_optimal_pos.c
@@ -8,8 +8,7 @@
 void test_choose_optimal_pos() {
     struct board_t* board = init_board();
     struct position_t* optimal_pos = choose_optimal_pos(board);
-    unsigned int x = PX(optimal_pos);
-    unsigned int y = PY(optimal_pos);
+    (void) optimal_pos;
     free_board(board);
 }
 
